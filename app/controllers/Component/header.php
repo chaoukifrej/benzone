1f9208c8259bf2d4a1e64ab4afd791ba5c60e6b1
Chaouki FREJ <chaoukifrej@hotmail.com>
2021-04-10 18:54:36 +0200

    travail sur Header, ajout fontFamily Roboto

---
+++ app/controllers/Component/header.php
@@ -0,0 +1,13 @@
+<?php
+?>
+<link rel="stylesheet" href="assets/styles/style.css" />
+<link rel="stylesheet" href="assets/styles/header.css">
+<div class="header">
+  <a href="#"><img class="headerLogo" src="assets/headerLogo.png" alt="logo Benzone"></a>
+  <nav>
+    <a class="headerLink" href="#">ACCUEIL</a>
+    <a class="headerLink" href="#">VENDRE</a>
+    <a class="headerLink" href="#">CONTACT</a>
+  </nav>
+  <a class="headerLink connexion" href="#">CONNEXION</a>
+</div>
