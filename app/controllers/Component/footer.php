7f6cfaddcefc97ed1c2f7bd72bf2c2a557444ba8
alexandre Dalmar <alexandredalmar@gmail.com>
2021-04-10 21:40:18 +0200

    creation du footer

---
+++ app/controllers/Component/footer.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/assets/styles/style.css" />
+    <link rel="stylesheet" href="/assets/styles/footer.css" />
+    <title>Document</title>
+</head>
+
+<body>
+
+    <div class="test">
+        <p>,nfmlaeknlezkflekaz</p>
+    </div>
+    <footer id="footer">
+        <ul>
+            <h3>pages</h3>
+            <li>accueil</li>
+            <li>encheres</li>
+            <li>vendre</li>
+            <li>connexion</li>
+        </ul>
+
+        <ul>
+            <h3>Encheres</h3>
+            <li>Vendre</li>
+            <li>Conseils de vente</li>
+            <li>Comment ca marche</li>
+            <li>Question fréquentes</li>
+            <li>Acheter une voiture aux encheres</li>
+            <li>Vendre une voiture aux encheres</li>
+        </ul>
+
+        <ul>
+            <h3>A propos</h3>
+            <li>A propos de BENZONE</li>
+            <li>Politique de confidentialité</li>
+            <li>Contact</li>
+        </ul>
+
+        <ul>
+            <h3>Langue</h3>
+            <li><img src="./assets/flag_anglais.ico" height="20px" class="flag">Anglais</li>
+            <li><img src="./assets/flag_russie.ico" height="20px" class="flag">Russe</li>
+            <li><img src="./assets/flag_belge.ico" height="20px" class="flag">Flamand</li>
+            <li><img src="./assets/flag_suede.ico" height="20px" class="flag">Italien</li>
+        </ul>
+    </footer>
+</body>
+
+</html>
