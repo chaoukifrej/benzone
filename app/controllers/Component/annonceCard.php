2e73bc8040208a112ced52bbd6d2aecd6c421554
Chaouki FREJ <chaoukifrej@hotmail.com>
2021-04-10 23:39:37 +0200

    debut sur card

---
+++ app/controllers/Component/annonceCard.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controllers\Component;
+
+class AnnonceCard
+{
+  protected $annonce = [
+    'id' => 1,
+    'first_price' => 1000,
+    'actual_price' => 3000,
+    'start_date' => '',
+    'final_date' => '',
+    'description' => 'blablabla',
+    'picture' => 'https://benzin.fr/auctions/show/911-type-997-carrera-s-cabriolet-2006-606341a16392d',
+    'brand' => 'Porsche',
+    'model' => 'blabla'
+  ];
+}
