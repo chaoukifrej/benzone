0ad08604fe99635663d517175105d7f382143180
chevillon loic <chevillonloic@gmail.com>
2021-04-10 19:45:44 +0200

    début page login

--- app/controllers/login.php
+++ app/controllers/login.php
@@ -24,14 +24,42 @@ class Login
             <meta charset="utf-8">
             <title>Démo routeur V1</title>
 
-            <link rel="stylesheet" type="text/css" href="assets/styles/style.css" />
+            <link rel="stylesheet" href="./assets/styles/style.css" />
+            <link rel="stylesheet" href="./assets/styles/login.css" />
         </head>
 
         <body>
             <div id="mainContainer">
-                <h1>Page connexion</h1>
-                <p>Bienvenue</p>
+                <h1 id="title">Page connexion</h1>
+                <form id="formConnectionInscription" action="" method="">
+                    <div id="connection">
+                        <label for="">Email</label>
+                        <input type="text" name="">
+
+                        <label for="">Mot de passe</label>
+                        <input type="password" name="">
+                    </div>
+
+                    <div id="inscription">
+                        <label for="">Nom</label>
+                        <input type="text" name="">
+
+                        <label for="">Prénom</label>
+                        <input type="text" name="">
+
+                        <label for="">Email</label>
+                        <input type="text" name="">
+
+                        <label for="">Mot de passe</label>
+                        <input type="password" name="">
+                    </div>
+
+                    <input type="submit">
+
+                    <p>Pas de compte ? <button id="btnConnectionToInscription">inscrivez-vous</button> !</p>
+                </form>
             </div>
+            <script type="text/javascript" src="./assets/js/login.js"></script>
         </body>
 
         </html>
---
+++ assets/js/login.js
@@ -0,0 +1,12 @@
+let btnConnectionToInscription = document.getElementById("btnConnectionToInscription")
+let connection = document.getElementById("connection")
+let inscription = document.getElementById("inscription")
+
+inscription.style.display = "none"
+
+btnConnectionToInscription.addEventListener('click', () => {
+
+    inscription.style.display = "block"
+
+})
+
