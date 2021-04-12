516b0284b552f76017b0a7bf98cda8348d9b4194
alexandre Dalmar <alexandredalmar@gmail.com>
2021-04-12 12:04:33 +0200

    page perso en commentaire

--- app/controllers/perso.php
+++ app/controllers/perso.php
@@ -15,14 +15,14 @@ use App\Controllers\Component\Menu;
 include  __DIR__ . "/../core/database.php";
 
 
-$lastname = filter_var($_POST["lastname"], FILTER_SANITIZE_STRING);
+/*  $lastname = filter_var($_POST["lastname"], FILTER_SANITIZE_STRING);
 $firstname = filter_var($_POST["firstname"], FILTER_SANITIZE_STRING);
 $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
-$password = filter_var($_POST["password"], FILTER_SANITIZE_STRING);
+$password = filter_var($_POST["password"], FILTER_SANITIZE_STRING); 
 
 
 $query = $dbh->prepare("UPDATE users (firstname, lastname, email, password) VALUES (?, ?, ?, ?)");
-$result = $query->execute([$lastname, $firstname, $email, $password]);
+$result = $query->execute([$lastname, $firstname, $email, $password]); */
 
 
 
