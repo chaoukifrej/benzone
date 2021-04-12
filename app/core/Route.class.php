20b038d8ddce4f19a1621a8278778107cf0e7494
Chaouki FREJ <chaoukifrej@hotmail.com>
2021-04-12 20:37:10 +0200

    route.class

--- app/core/Route.class.php
+++ app/core/Route.class.php
@@ -20,7 +20,7 @@ class Route
     /* Propriétés */
     protected $path; // Chemin de la route
     protected $callback; // Fonction callback à appeler si la route correspond
-
+    protected $dynamicValues = [];
 
     /**
      * Constructeur
@@ -36,7 +36,23 @@ class Route
      */
     public function check($uri)
     {
-        return $uri === $this->path;
+        /* Vérifier si la route est dynamique*/
+        $dynamic_part = [];
+        preg_match("/:.*/", $this->path, $dynamic_part, PREG_OFFSET_CAPTURE);
+
+        if (count($dynamic_part) > 0) {
+            $name = substr($dynamic_part[0][0], 1); // Nom de la variable de la route dynamique
+            $value = substr($uri, $dynamic_part[0][1]); // Valeur de la variable de la route dynamique
+
+            /* Stockage de la valeur dynamique */
+            $this->dynamicValues[$name] = $value;
+
+            /* Comparaison pour une route dynamique */
+            return substr($this->path, 0, $dynamic_part[0][1]) === substr($uri, 0, $dynamic_part[0][1]);
+        } else {
+            /* Comparaison pour une route statique */
+            return $uri === $this->path;
+        }
     }
 
     /**
@@ -44,6 +60,6 @@ class Route
      */
     public function call()
     {
-        call_user_func($this->callback); // Appel la fonction callback stokée dans la propriété
+        call_user_func($this->callback, $this->dynamicValues); // Appel la fonction callback stokée dans la propriété
     }
 }
