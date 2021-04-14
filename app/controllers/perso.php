816b102b594389d50f15d1c69d06a2c13cf425bd
alexandre Dalmar <alexandredalmar@gmail.com>
2021-04-14 11:26:52 +0200

    test

--- app/controllers/perso.php
+++ app/controllers/perso.php
@@ -11,39 +11,19 @@ namespace App\Controllers;
 
 include __DIR__ . '/../views/perso.view.php';
 include __DIR__ . "/../models/Users.model.php";
+include __DIR__ . "/../models/Adverts.model.php";
+
 
 use App\Views\Perso as PersoView;
 use App\Controllers\Component\Menu;
 use App\Models\Users;
+use App\Models\Adverts;
 
 
 class Perso
 {
 
     protected $advert;
-
-    public function countTotal()
-    {
-        $stock = 0;
-        foreach ($this->advert as $key => $value) {
-            foreach ($value as $k => $v) {
-                if ($k == "actual_price") {
-                    $stock += (int)$v;
-                }
-            }
-        }
-
-        return $stock;
-    }
-
-    public function winAdvert()
-    {
-        //. Connexion Base de données
-        include  __DIR__ . "/../core/database.php";
-
-        $this->advert = $dbh->query("SELECT c.model , c.brand, a.actual_price , a.id FROM adverts a INNER JOIN car c ON c.id = a.car_id WHERE a.bidder_id = $_SESSION[id]")->fetchAll(\PDO::FETCH_ASSOC);
-    }
-    // SELECT  a.id, a.actual_price, a.final_date, a.description, c.model, c.brand, c.power FROM adverts a INNER JOIN car c ON c.id = a.car_id WHERE a.bidder_id = 39
 
     /* fonction modification base de données */
 
